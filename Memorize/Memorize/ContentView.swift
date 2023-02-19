b37bc866f0ecc92855656c9b2cd98062c4aa4253
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2023-02-19 23:12:00 +0200

    More SwiftUI
    Create view struct to be reused

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -9,27 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
         HStack{
-            ZStack {
-                
-                RoundedRectangle(cornerRadius: 25)
-                    .stroke(lineWidth: 3)
-                Text("Hello, world")
-            }
-            .padding(.horizontal)
-            .foregroundColor(.red)
-            
-            ZStack {
-                
-                RoundedRectangle(cornerRadius: 25)
-                    .stroke(lineWidth: 3)
-                Text("Hello, world")
-            }
-            .padding(.horizontal)
-            .foregroundColor(.red)
+            CardView()
+            CardView()
+            CardView()
         }
-        
+    }
+}
+
+struct CardView: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 25)
+                .stroke(lineWidth: 3)
+            Text("Hello, world")
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
     }
 }
 
