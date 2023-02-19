003fd48d6bac5938922f5834b1f023fa0537c725
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2023-02-19 19:56:38 +0200

    Getting started with SwiftUI
    ZStack:
    places views on top of each other
    
    Everything is built up from View.
    
    No need to type extra info when it's clear such as Color.red, only .red is enough.

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            
+            RoundedRectangle(cornerRadius: 25)
+                .stroke(lineWidth: 3)
+            Text("Hello, world")
         }
-        .padding()
+        .padding(.horizontal)
+        .foregroundColor(.red)
     }
 }
 
