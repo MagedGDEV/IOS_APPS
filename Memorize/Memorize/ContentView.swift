044bdec1c27634cbb461fe4b2d0602688c18e44a
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2023-02-28 12:42:57 +0200

    Prepare for lecture 3
    Delete extra content done in the assignment to work with the lecture

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -8,84 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    var vehicles = ["🚗", "🚕", "🚙", "🚌", "🚎", "🏎️", "🚓", "🚑",
-                    "🚒", "🚐", "🛻", "🚚", "🚛", "🚜", "🦽", "🦼",
-                    "🛴", "🚲", "🛵", "🏍️", "🛺", "🚡", "🚠", "🚃"]
-    var smileys = ["😀", "😃", "😄", "😁", "😆", "🥹", "😅", "😂",
-                   "🤣", "🥲", "☺️", "😊", "😇", "🙂", "🙃", "😉",
-                   "😌", "😍", "🥰", "😘", "😗", "😙", "😚", "😋",
-                   "😛", "😝", "😜", "🤪", "🤨", "🧐", "🤓", "😎"]
-    var animals = ["🐶", "🐱", "🐭", "🐹", "🐰", "🦊", "🐻", "🐼",
+
+    var emojis = ["🐶", "🐱", "🐭", "🐹", "🐰", "🦊", "🐻", "🐼",
                    "🐻‍❄️", "🐨", "🐯", "🦁", "🐮", "🐷", "🐸", "🐵",
                    "🐔", "🐧", "🐦", "🐤", "🦆", "🦅", "🦉", "🦇",
                    "🐺", "🐗", "🐴", "🦄", "🐊"]
-
-    @State var emojis: [String]
-    @State var emojiCount: Int
-    
-    
-    init (){
-        emojis = vehicles
-        emojiCount = vehicles.count
-    }
     
     var body: some View {
         VStack{
-            Text("Memorize!").font(.largeTitle)
             ScrollView{
                 LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]){
-                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                    ForEach(emojis[0..<emojis.count], id: \.self) { emoji in
                         CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
                     }
                 }
             }
             .foregroundColor(.red)
-            Spacer()
-            HStack {
-                vehicleButton
-                Spacer()
-                simleyButton
-                Spacer()
-                animalButton
-            }
-            .foregroundColor(.blue)
-            .padding(.horizontal)
         }
         .padding(.horizontal)
-    }
-    
-    var vehicleButton: some View {
-        Button {
-            emojis = vehicles.shuffled()
-            emojiCount = Int.random(in: 1...vehicles.count)
-        } label: {
-            VStack {
-                Image(systemName: "car").font(.largeTitle)
-                Text("Vehicles")
-            }
-        }
-    }
-    var simleyButton: some View {
-        Button {
-            emojis = smileys.shuffled()
-            emojiCount = Int.random(in: 1...smileys.count)
-        } label: {
-            VStack {
-                Image(systemName: "face.smiling").font(.largeTitle)
-                Text("Simleys")
-            }
-        }
-    }
-    var animalButton: some View {
-        Button {
-            emojis = animals.shuffled()
-            emojiCount = Int.random(in: 1...animals.count)
-        } label: {
-            VStack {
-                Image(systemName: "pawprint").font(.largeTitle)
-                Text("Animals")
-            }
-        }
     }
 }
 
@@ -118,13 +58,6 @@ struct CardView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
-            .previewDevice(PreviewDevice(rawValue: "iPhone 14"))
-            .previewDisplayName("iPhone 14")
-
-        ContentView()
-            .previewDevice(PreviewDevice(rawValue: "iPhone 14 Pro Max"))
-            .previewDisplayName("iPhone 14 Pro Max")
         
         ContentView()
             .previewDevice(PreviewDevice(rawValue: "iPhone 11"))
