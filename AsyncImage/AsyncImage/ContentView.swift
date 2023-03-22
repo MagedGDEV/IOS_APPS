e3bc94db1069d2614a862f16d4d547486b9881d3
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2023-03-22 23:11:45 +0200

    scale the image
    Increasing the value of scale will shrink down the images

--- AsyncImage/AsyncImage/ContentView.swift
+++ AsyncImage/AsyncImage/ContentView.swift
@@ -11,7 +11,9 @@ struct ContentView: View {
     private let imageURL: String = "https://credo.academy/credo-academy@3x.png"
     var body: some View {
         // MARK: - 1: BASIC
-        AsyncImage(url: URL(string: imageURL))
+        //AsyncImage(url: URL(string: imageURL))
+        // MARK: - 2: SCALE
+        AsyncImage(url: URL(string: imageURL), scale: 3.0)
     }
 }
 
