1aa77af0d54fae02d3b9e04ade4252807e922f73
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2023-03-22 23:34:12 +0200

    add placeholder

--- AsyncImage/AsyncImage/ContentView.swift
+++ AsyncImage/AsyncImage/ContentView.swift
@@ -13,7 +13,22 @@ struct ContentView: View {
         // MARK: - 1: BASIC
         //AsyncImage(url: URL(string: imageURL))
         // MARK: - 2: SCALE
-        AsyncImage(url: URL(string: imageURL), scale: 3.0)
+        //AsyncImage(url: URL(string: imageURL), scale: 3.0)
+        // MARK: - 3: PLACEHOLDER
+        AsyncImage(url: URL(string: imageURL), scale: 3.0) {
+            image in image
+                .resizable()
+                .scaledToFit()
+        } placeholder: {
+            Image(systemName: "photo.circle.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: 120)
+                .foregroundColor(.purple)
+                .opacity(0.5)
+        }
+        .padding(40)
+        
     }
 }
 
