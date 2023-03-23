b9bf73e4afe732534cfe1d4d6b8ab537491cdfa8
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2023-03-24 01:43:54 +0200

    Add the header section

--- Restart/Restart/Screens/OnboardingView.swift
+++ Restart/Restart/Screens/OnboardingView.swift
@@ -20,6 +20,23 @@ struct OnboardingView: View {
             VStack(spacing: 20) {
                 // MARK: - HEADER
                 
+                Spacer()
+                VStack(){
+                    Text("Share.")
+                        .font(.system(size:60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    
+                        Text("""
+                        It's not how much we give but
+                        how much love we put into giving
+                        """)
+                        .font(.title3)
+                        .fontWeight(.light)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 10)
+                }
                 
                 // MARK: - CENTER
                 
