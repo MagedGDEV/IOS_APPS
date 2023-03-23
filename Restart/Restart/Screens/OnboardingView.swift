d5e997b17661fa374ad43a17c23a57bf6f39e0b5
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2023-03-24 01:37:23 +0200

    Add color to the background
    using ignore the safe area will color the whole app

--- Restart/Restart/Screens/OnboardingView.swift
+++ Restart/Restart/Screens/OnboardingView.swift
@@ -8,15 +8,26 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    // MARK: - PROPERTY
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    
+    // MARK: - BODY
+    
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Onboarding")
-                .font(.largeTitle)
-            Button(action: {
-                isOnboardingViewActive = false
-            }) {
-                Text("Start")
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
+            VStack(spacing: 20) {
+                // MARK: - HEADER
+                
+                
+                // MARK: - CENTER
+                
+                
+                // MARK: - FOOTER
+                
+                
+                
             }
         }
     }
