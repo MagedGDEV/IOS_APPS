db70d811ab290352b69dc70b02188d4658290f84
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2023-03-24 15:06:18 +0200

    center animation

--- Restart/Restart/Screens/OnboardingView.swift
+++ Restart/Restart/Screens/OnboardingView.swift
@@ -51,7 +51,8 @@ struct OnboardingView: View {
                     Image("character-1")
                         .resizable()
                         .scaledToFit()
-                }
+                        .opacity(isAnimating ? 1 : 0)
+                        .animation(.easeOut(duration: 1), value: isAnimating)                }
                 Spacer()
                 
                 // MARK: - FOOTER
