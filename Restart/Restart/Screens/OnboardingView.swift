a892684134765d87d2964ac46edc89b52116e298
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2023-03-27 16:39:59 +0200

    Add animation to the rings

--- Restart/Restart/Screens/OnboardingView.swift
+++ Restart/Restart/Screens/OnboardingView.swift
@@ -44,11 +44,14 @@ struct OnboardingView: View {
                 .opacity(isAnimating ? 1 : 0)
                 .offset(y: isAnimating ? 0 : -40)
                 .animation(.easeOut(duration: 1), value: isAnimating)
-                
                 // MARK: - CENTER
                 
                 ZStack {
                     CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
+                        .offset(x: -imageOffset.width)
+                        .blur(radius: abs(imageOffset.width)/5)
+                        .animation(.easeOut(duration: 1), value: imageOffset)
+                    
                     Image("character-1")
                         .resizable()
                         .scaledToFit()
