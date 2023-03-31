ce57c1b09c7221ff11328df875fcb4bf560aae10
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2023-04-01 00:20:27 +0200

    Project files

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Maged Alosali on 01/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
