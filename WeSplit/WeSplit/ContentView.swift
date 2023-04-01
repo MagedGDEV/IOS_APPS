62ad5d18b619b29853116b5985f3db530a9abc32
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2023-04-01 23:38:09 +0200

    Add a title

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -11,21 +11,24 @@ struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     var body: some View {
-        Form {
-            Section {
-                TextField("Amount", value: $checkAmount, format:
-                        .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                .keyboardType(.decimalPad)
-                Picker ("Number of people", selection: $numberOfPeople){
-                    ForEach (2..<100){
-                        Text ("\($0) people")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format:
+                            .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    .keyboardType(.decimalPad)
+                    Picker ("Number of people", selection: $numberOfPeople){
+                        ForEach (2..<100){
+                            Text ("\($0) people")
+                        }
                     }
                 }
+                Section {
+                    Text(checkAmount, format:
+                            .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
             }
-            Section {
-                Text(checkAmount, format:
-                        .currency(code: Locale.current.currency?.identifier ?? "USD"))
-            }
+            .navigationTitle("WeSplit")
         }
     }
 }
