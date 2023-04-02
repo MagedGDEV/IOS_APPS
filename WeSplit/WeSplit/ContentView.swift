99f7c1b4f8cae7c020df44b3b35322c077a463d0
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2023-04-02 21:17:47 +0200

    Add tip percentage view

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -10,6 +10,11 @@ import SwiftUI
 struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [0, 10, 15, 20, 25]
+
+    
     var body: some View {
         NavigationView {
             Form {
@@ -22,6 +27,16 @@ struct ContentView: View {
                             Text ("\($0) people")
                         }
                     }
+                }
+                Section {
+                    Picker ("Tip percentage", selection: $tipPercentage){
+                        ForEach (tipPercentages, id: \.self){
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text ("How much tip you want to leave")
                 }
                 Section {
                     Text(checkAmount, format:
