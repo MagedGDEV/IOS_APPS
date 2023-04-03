bf63b460fae942b3a83079e3cf8c37598f01dd6c
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2023-04-03 15:25:59 +0200

    Add linear gradient of colors

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,22 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            VStack(spacing: 0){
-                Color.red
-                Color.blue
-            }
-            Text("your content")
-                .foregroundStyle(.secondary)
-                .padding(50)
-                .background(.ultraThinMaterial)
-        }
-        .ignoresSafeArea()
+        LinearGradient(gradient: Gradient(colors: [.white, .black]), startPoint: .top, endPoint: .bottom)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
+    static var previews: some View { 
         ContentView()
     }
 }
