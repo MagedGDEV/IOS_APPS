5784a7f5f7a3646c49e1725de68faa808423bd42
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2023-04-03 16:36:28 +0200

    Add the project assests

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,22 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingAlert = false
+
     var body: some View {
-        Button("Show alert") {
-            showingAlert = true
-        }
-        .alert("Important message", isPresented: $showingAlert){
-            Button("Delete", role: .destructive){}
-            Button("Cancel", role: .cancel){}
-        } message: {
-            Text("Please read this")
-        }
+        Text ("Hello")
     }
-    
-    func executeDelete () {
-        print("Now deleting")
-    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
