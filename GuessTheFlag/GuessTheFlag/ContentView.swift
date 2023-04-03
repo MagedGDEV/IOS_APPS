8fa5b2ed84e2cdf29f4d61284c5fb84191e6aff7
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2023-04-03 17:30:21 +0200

    Insert radial gradient and add spacing

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -16,27 +16,50 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            LinearGradient (gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+            RadialGradient (stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+            ], center: .top, startRadius: 200, endRadius: 700)
                 .ignoresSafeArea()
-            VStack (spacing: 30) {
-                VStack {
-                    Text ("Tap the flag of")
-                        .font(.subheadline.weight(.heavy))
-                    Text (countries[correctAnswer])
-                        .font(.largeTitle.weight(.semibold))
-                }
-                .foregroundColor(.white)
-                ForEach (0..<3) { number in
-                    Button {
-                        flagTapped(number)
-                    } label: {
-                        Image (countries[number])
-                            .renderingMode(.original)
-                            .clipShape(Capsule())
-                            .shadow(radius: 10)
+            VStack {
+                Spacer()
+                Text ("Guess the Flag")
+                    .font(.largeTitle.bold())
+                VStack (spacing: 15) {
+                    VStack {
+                        Text ("Tap the flag of")
+                            .font(.subheadline.weight(.heavy))
+                            .foregroundColor(.secondary)
+                            .foregroundStyle(.secondary)
+                        Text (countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                            .foregroundColor(.primary)
+                    }
+
+                    ForEach (0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image (countries[number])
+                                .renderingMode(.original)
+                                .clipShape(Capsule())
+                                .shadow(radius: 10)
+                        }
                     }
                 }
+                .frame (maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                Spacer()
+                Spacer()
+                Text ("Score: \(score)")
+                    .font(.title.bold())
+                Spacer()
+
             }
+            .foregroundColor(.white)
+            .padding()
         }
         .alert(scoreTitle, isPresented: $showingScore){
             Button("Continue", action: askQuestion)
