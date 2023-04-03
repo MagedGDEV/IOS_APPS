7e98a50ae27ecea7de6a19d2296ad607e3c3e717
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2023-04-03 15:29:23 +0200

    Add custom stops

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,12 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        LinearGradient(gradient: Gradient(colors: [.white, .black]), startPoint: .top, endPoint: .bottom)
+        LinearGradient(gradient: Gradient(stops: [
+            .init (color: .white, location: 0.45),
+            .init (color: .black, location: 0.55)
+        ]), startPoint: .top, endPoint: .bottom)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View { 
+    static var previews: some View {
         ContentView()
     }
 }
