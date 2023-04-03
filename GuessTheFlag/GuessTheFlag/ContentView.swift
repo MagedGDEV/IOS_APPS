3e8092e042178311f6004dca89295d84e511e180
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2023-04-03 16:26:53 +0200

    Showing alert

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,12 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
     var body: some View {
-        Button {
-            print("Button was tapped!")
-            
-        } label: {
-            Label ("Edit", systemImage: "pencil")
+        Button("Show alert") {
+            showingAlert = true
+        }
+        .alert("Important message", isPresented: $showingAlert){
+            Button("Delete", role: .destructive){}
+            Button("Cancel", role: .cancel){}
+        } message: {
+            Text("Please read this")
         }
     }
     
