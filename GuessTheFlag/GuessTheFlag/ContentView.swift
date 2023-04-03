70aba6c23a08c41b2176b47bc022cd73ebcab08f
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2023-04-03 15:13:08 +0200

    To color the whole app even the safe areas

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -10,8 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            Color (red: 1, green: 0.8, blue: 0)
+            Color (red: 1, green: 0.8, blue: 0 ) 
         }
+        .ignoresSafeArea()
     }
 }
 
