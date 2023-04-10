167be67dc9f790845ff55c2e820145bf015696ce
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2023-04-10 21:39:33 +0200

    the start point of the project

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -9,17 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        List(0..<5) {
-            Text ("Dynamic row \($0)")
-        }
-    }
-    
-    func loadFile() {
-        if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt"){
-            if let fileContent = try? String(contentsOf: fileURL) {
-                print(fileContent)
-            }
-        }
+        Text ("Hello, world")
     }
 }
 
