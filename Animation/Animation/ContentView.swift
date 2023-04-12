02b082cae7ad47741f09b8e07715e393933b18ab
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2023-04-12 23:09:36 +0200

    apply overlay animation

--- Animation/Animation/ContentView.swift
+++ Animation/Animation/ContentView.swift
@@ -11,18 +11,25 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
     var body: some View {
         Button("Tap me") {
-            animationAmount += 1
+            // animationAmount += 1
         }
         .padding(50)
         .foregroundColor(.white)
         .background(.red)
         .clipShape(Circle())
-        .scaleEffect(animationAmount)
-        .blur(radius:(animationAmount - 1) * 3)
-        .animation(
-            .easeInOut(duration: 1)
-            .repeatCount(3, autoreverses: true),
-            value: animationAmount)
+        .overlay {
+            Circle()
+                .stroke(.red)
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+                .animation(
+                    .easeInOut(duration: 1)
+                    .repeatForever(autoreverses: false),
+                    value: animationAmount)
+        }
+        .onAppear{
+            animationAmount = 2
+        }
     }
 }
 
