7d3f72cc3015bfec1b4510e9c3b8dec7d920e8db
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2023-04-12 23:02:02 +0200

    add animation with specific type
    that takes 2 seconds and starts after clicking by 1 second

--- Animation/Animation/ContentView.swift
+++ Animation/Animation/ContentView.swift
@@ -19,7 +19,10 @@ struct ContentView: View {
         .clipShape(Circle())
         .scaleEffect(animationAmount)
         .blur(radius:(animationAmount - 1) * 3)
-        .animation(.default, value: animationAmount)
+        .animation(
+            .easeInOut(duration: 2)
+            .delay(1),
+            value: animationAmount)
     }
 }
 
