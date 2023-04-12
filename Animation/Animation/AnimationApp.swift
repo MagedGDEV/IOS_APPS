02961a9f23a52ab1018606f612680ab8705c7026
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2023-04-12 02:13:47 +0200

    Create new project

---
+++ Animation/Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Maged Alosali on 12/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
