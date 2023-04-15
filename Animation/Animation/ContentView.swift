ad26b5f449aa61335c2a0c7f32b161e25e36a5c4
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2023-04-15 21:47:20 +0200

    Add a gesture and animation to string
    to create a beautiful effect when the text is dragged

--- Animation/Animation/ContentView.swift
+++ Animation/Animation/ContentView.swift
@@ -8,26 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    let letters = Array("Hello, World")
     @State private var dragAmount = CGSize.zero
+    @State private var enabled = false
     var body: some View {
-        LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
-            .frame(width: 300, height: 200)
-            .clipShape(RoundedRectangle(cornerRadius: 40))
-            .offset(dragAmount)
-            .gesture(
-                DragGesture()
-                    .onChanged {
-                        dragAmount = $0.translation
-                    }
-                    .onEnded { _ in
-                        withAnimation {
-                            dragAmount = .zero
-                        }
-                    }
-            )
-            
-            
-        
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count, id: \.self){ num in
+                Text(String(letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enabled ? .blue : .red)
+                    .offset(dragAmount)
+                    .animation(.default.delay(Double(num)/20), value: dragAmount)
+                    
+            }
+        }
+        .gesture(
+            DragGesture()
+                .onChanged {
+                    dragAmount = $0.translation
+                }
+                .onEnded { _ in
+                    dragAmount = .zero
+                    enabled.toggle()
+                }
+        )
     }
 }
 
