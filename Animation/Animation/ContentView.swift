56d97fb53d04d89b7bb941ece71fe4c38cf144b2
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2023-04-15 22:21:04 +0200

    Add custom transition effect

--- Animation/Animation/ContentView.swift
+++ Animation/Animation/ContentView.swift
@@ -7,20 +7,43 @@
 
 import SwiftUI
 
+struct CornerRadiusModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body (content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(active: CornerRadiusModifier(amount: -90, anchor: .topLeading),
+                  identity: CornerRadiusModifier(amount:0, anchor: .topLeading))
+    }
+}
+
 struct ContentView: View {
     @State private var isShowing = false
     var body: some View {
-        VStack {
-            Button ("Tap me") {
-                withAnimation() {
-                    isShowing.toggle()
-                }
-            }
+        ZStack {
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 200, height: 200)
+                
+            
             if isShowing {
                 Rectangle()
                     .fill(.red)
                     .frame(width: 200, height: 200)
-                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+                    .transition(.pivot)
+            }
+        }
+        .onTapGesture {
+            withAnimation {
+                isShowing.toggle()
             }
         }
     }
