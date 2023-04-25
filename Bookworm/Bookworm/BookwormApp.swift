93a692af9acda768e6878ae2bba5dc24896330b7
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2023-04-25 20:04:15 +0200

    Create project files

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Maged Alosali on 25/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
