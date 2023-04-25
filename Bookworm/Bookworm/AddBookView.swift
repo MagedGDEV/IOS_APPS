995ef3eb63719a1b0e14827c72b98e199a7afaaa
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2023-04-25 21:49:58 +0200

    create a view that has the stored managedObject

---
+++ Bookworm/Bookworm/AddBookView.swift
@@ -0,0 +1,21 @@
+//
+//  AddBookView.swift
+//  Bookworm
+//
+//  Created by Maged Alosali on 25/04/2023.
+//
+
+import SwiftUI
+
+struct AddBookView: View {
+    @Environment(\.managedObjectContext) private var moc
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddBookView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddBookView()
+    }
+}
