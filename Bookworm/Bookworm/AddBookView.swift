73ed63c2816ac1226b316ca099c399564b6ba7c4
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2023-04-25 22:18:29 +0200

    Create the ui of addbook view

--- Bookworm/Bookworm/AddBookView.swift
+++ Bookworm/Bookworm/AddBookView.swift
@@ -9,8 +9,48 @@ import SwiftUI
 
 struct AddBookView: View {
     @Environment(\.managedObjectContext) private var moc
+    
+    @State private var title = ""
+    @State private var author = ""
+    @State private var genre = ""
+    @State private var review = ""
+    @State private var rating = 0
+    
+    private let genres = ["Fantasy", "Horror", "Kids", "Mystery", "Poetry", "Romance", "Thriller"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Name of the book", text: $title)
+                    TextField("Author's name", text: $author)
+                    Picker("Genre", selection: $genre){
+                        ForEach(genres, id:\.self){
+                            Text($0)
+                        }
+                    }
+                }
+                
+                Section {
+                    TextEditor(text: $review)
+                    
+                    Picker("Rating", selection: $rating){
+                        ForEach(0..<6){
+                            Text("\($0)")
+                        }
+                    }
+                } header: {
+                    Text("Write a review")
+                }
+                
+                Section {
+                    Button("Save") {
+                        
+                    }
+                }
+            }
+            .navigationTitle("Add Book")
+        }
     }
 }
 
