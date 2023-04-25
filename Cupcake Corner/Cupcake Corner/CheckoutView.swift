e8078a39bf6a6af644aaa0ae381ffd18f047e0a2
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2023-04-25 14:20:57 +0200

    Add the UI of the address view and link it to the checkout view

---
+++ Cupcake Corner/Cupcake Corner/CheckoutView.swift
@@ -0,0 +1,21 @@
+//
+//  CheckoutView.swift
+//  Cupcake Corner
+//
+//  Created by Maged Alosali on 25/04/2023.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    @ObservedObject var order: Order
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckoutView(order: Order())
+    }
+}
